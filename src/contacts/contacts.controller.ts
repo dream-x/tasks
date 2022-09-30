b4013fc7ccec097e6d8b790502fd55dacc5e7ba1
k2peridotdev <k2peridotcrystal@gmail.com>
2022-09-30 06:40:31 -0400

    Implement idempotency

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -7,18 +7,39 @@ import {
   Param,
   Delete,
   Header,
+  Headers,
+  Res,
+  BadRequestException,
 } from '@nestjs/common';
+import type { Response } from 'express';
 import { ContactsService } from './contacts.service';
 import { CreateContactDto } from './dto/create-contact.dto';
 import { UpdateContactDto } from './dto/update-contact.dto';
 
 @Controller('contacts')
 export class ContactsController {
+  dummyCache = {};
+
   constructor(private readonly contactsService: ContactsService) {}
 
   @Post()
-  create(@Body() createContactDto: CreateContactDto) {
-    return this.contactsService.create(createContactDto);
+  async create(
+    @Headers('x-idempotence-key') idemKey: string,
+    @Body() createContactDto: CreateContactDto,
+    @Res() res: Response,
+  ) {
+    if (!idemKey) {
+      throw new BadRequestException(`Idempotence key required`);
+    }
+
+    if (this.dummyCache[idemKey]) {
+      return res.status(304).send('Not Modified');
+    }
+
+    const newContact = await this.contactsService.create(createContactDto);
+    this.dummyCache[idemKey] = newContact;
+
+    return res.status(201).json(newContact);
   }
 
   @Header('Content-Type', 'text/csv')
