c502fbcf6e85faa99620bbf8034dff18c1573d8f
Mahmoud Bakr <mbakr6821@gmail.com>
2022-10-28 03:35:54 +0200

    Create: the needed routes under namespaces api + v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  namespace :api do
+    namespace :v1 do
+      get '/contacts', to: 'contacts#index'
+      post '/create/contact', to: 'contacts#create'
+      get '/contacts/:id', to: 'contacts#show'
+      put '/contacts/:id', to: 'contacts#update'
+      delete '/contacts/:id', to: 'contacts#destroy'
+    end
+  end
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
