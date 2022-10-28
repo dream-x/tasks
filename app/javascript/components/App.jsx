b5804f7d446d2b470209ee953fb34888d0488877
Mahmoud Bakr <mbakr6821@gmail.com>
2022-10-28 04:38:01 +0200

    Create: AddContact component + POST request

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Contact from './Contact';
 import Contacts from './Contacts';
+import AddContact from './AddContact';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route exact path="/" element={<Contacts />} />
         <Route exact path="/contacts/:id" element={<Contact />} />
+        <Route exact path="/new/contact" element={<AddContact />} />
       </Routes>
       </BrowserRouter>
   )
