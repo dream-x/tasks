cc9595bfd1a2cc0c246f1d177e34636c4c4624c1
Mahmoud Bakr <mbakr6821@gmail.com>
2022-10-28 03:35:11 +0200

    Create: contacts controller with the REST methods + Edit: Contact model

---
+++ app/controllers/api/v1/contacts_controller.rb
@@ -0,0 +1,39 @@
+class Api::V1::ContactsController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
+    def index
+        @contacts = Contact.all.order(created_at: :desc)
+        respond_to do |format|
+            format.json { render json: @contacts }
+            format.csv { send_data @contacts.to_csv, filename: "#{@contacts.first.name}.csv" }
+        end
+    end
+
+    def show
+        @contact = Contact.find(params[:id])
+        respond_to do |format|
+            format.json { render json: @contact }
+        end
+    end
+
+    def create
+        @contact = Contact.new(contact_params)
+        @contact.save
+    end
+
+    def update
+        @contact = Contact.find(params[:id])
+        @contact.update(contact_params)
+    end
+
+    def destroy
+        @contact = Contact.find(params[:id])
+        @contact.destroy
+    end
+
+    private
+
+    def contact_params
+        params.require(:contact).permit(:name, :jon, :age)
+    end
+end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,15 @@
+require 'csv'
+
 class Contact < ApplicationRecord
+    def self.to_csv
+        attrs = %w{id name age job}
+
+        CSV.generate(headers: true) do |csv|
+            csv << attrs
+
+            all.find_each do |contact|
+                csv << attrs.map{ |attr| contact.send(attr)}
+            end
+        end
+    end
 end
