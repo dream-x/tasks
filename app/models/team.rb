78773475567df88df53ed89eb515a52aa966c88b
kwambiee <kwambokaj2.jk@gmail.com>
2022-11-04 21:18:01 +0300

    Add validation team model

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,13 @@
 require 'csv'
 
 class Team < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :games_played, presence: true, numericality: { only_integer: true }
+    validates :games_won, presence: true, numericality: { only_integer: true }
+    validates :games_drawn, presence: true, numericality: { only_integer: true }
+    validates :games_lost, presence: true, numericality: { only_integer: true }
+    validates :goal_difference, presence: true, numericality: { only_integer: true }
+
     def self.to_csv(make)
         attributes = %w{id name games_played games_won games_drawn games_lost goal_difference}
         CSV.generate(headers: true) do |csv|
