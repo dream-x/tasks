67ba5763eacdef80b7c9167b57bb4d1459a77a99
kwambiee <kwambokaj2.jk@gmail.com>
2022-11-04 20:14:48 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "teams#index"
+  resources :teams, only: [:index, :show, :create, :update, :destroy]
 end
