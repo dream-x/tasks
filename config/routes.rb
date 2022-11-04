8a1b11cac45a769380a76257624acd6713876617
kwambiee <kwambokaj2.jk@gmail.com>
2022-11-04 21:48:20 +0300

    Add home route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "teams#index"
+  root "homes#index"
   namespace :api do
     namespace :v1 do
       resources :teams, only: [:index, :show, :create, :update, :destroy]
