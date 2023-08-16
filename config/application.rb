5e56eda711e609c6380e6250535a68eb4c076c9b
Danish Rasheed <danishrashed@live.com>
2023-08-16 23:54:07 +0500

    added dotenv for environment variables

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,10 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+if Rails.env.development?
+  Dotenv::Railtie.load
+end
+
 module ProductAPI
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
