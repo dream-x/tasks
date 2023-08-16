6883ec79b9a63586c1583486822a0cdc7ee58994
Danish Rasheed <danishrashed@live.com>
2023-08-17 00:06:15 +0500

    changes in controller and model

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,44 +1,44 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :update, :destroy]
+  before_action :find_product, only: [:show, :update, :destroy]
 
-  def index
-    @products = Product.all
-    render json: @products
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      render json: @product, status: :created
+    else
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def show
     render json: @product
   end
 
-  def create
-    @product = Product.new(product_params)
-
-    if @product.save
-      render json: @product, status: :created
-    else
-      render json: @product.errors, status: :unprocessable_entity
-    end
+  def index
+    @products = Product.all
+    render json: @products
   end
 
   def update
     if @product.update(product_params)
       render json: @product
     else
-      render json: @product.errors, status: :unprocessable_entity
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def destroy
     @product.destroy
+    head :no_content
   end
 
   private
 
-  def set_product
-    @product = Product.find(params[:id])
-  end
-
   def product_params
     params.require(:product).permit(:name, :description, :price)
-  end  
+  end
+
+  def find_product
+    @product = Product.find(params[:id])
+  end
 end
